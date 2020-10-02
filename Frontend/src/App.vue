fc38b0043301bebd35d8853418c256508ff8b2be
Mahdi Karimi <mhdi_kr@outlook.com>
2020-10-02 11:02:24 +0330

    getting core

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,41 @@
 <template>
+<<<<<<< HEAD
   <div id="app">
       <p>welcome to this application</p>
+=======
+  <div id="app" class="container-fluid">
+    <!-- BLUE section -->
+    <div class="columns" >
+      <section class="hero full-height column is-info">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              SEO analyzer 1.0
+            </h1>
+            <h2 class="subtitle">
+              universal search engine optimization solution
+            </h2>
+            <div class="columns">
+              <div class="column">
+                <file-reader @load="text = $event"></file-reader>
+              </div>
+              <div class="column">
+                <analyze-btn />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- WHITE section -->
+      <section class="hero full-height column is-light">
+        <div class="hero-body">
+          <div class="container">
+            <code-block :code="text" v-if="text.length>0"></code-block>
+          </div>
+        </div>
+      </section>
+    </div>
+>>>>>>> 1e07e55... attempt to integrate core with the backend
   </div>
 </template>
 
---
+++ Frontend/src/components/AnalyzeBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="buttons">
+    <button class="button is-light is-block is-fullwidth" :class="isLoading ? 'is-loading' : '' " @click="analyze">Analyze</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'analyzeBtn',
+    data: () => {
+      return {
+        isLoading: false,
+      }
+    },
+    methods: {
+      analyze(){
+        console.log('hello!')
+        this.isLoading = true
+        fetch('www.dumas.ir:3000/analyze')
+            .then(response => response.json())
+            .then(json => {
+              console.log(json)
+              this.isLoading = false
+            })
+      }
+    }
+  }
+</script>
